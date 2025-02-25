44d87515b91121eb8e2c56c7dbd5059d4a2dc792
Joljolion <joaolazarini10@gmail.com>
2025-02-25 18:21:21 -0300

    Ajuste no formato para o github pages

---
+++ docs/scripts/script.js
@@ -0,0 +1,12 @@
+function homepage(){
+    window.location = "../../index.html"
+}
+
+let fatecanos = JSON.parse(localStorage.getItem("fatecanos"));
+
+let user = {
+    nome: "Alfredo",
+    email: "alfredo@gmail.com",
+    senha: "123",
+    telefone: "11 913383635",
+}
